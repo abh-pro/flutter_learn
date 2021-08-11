c0b2324a1e768545db20bebc82713a47a1a10cc5
abh-pro <abhishek.chebolu@gmail.com>
2021-08-12 00:09:23 +0530

    app shifting colors

--- lib/App.dart
+++ lib/App.dart
@@ -1,7 +1,8 @@
-import 'package:flutter/material.dart';
-import 'dart:math';
-import 'screens.dart';
 
+import 'dart:async';
+
+import "package:flutter/material.dart";
+import 'package:flutter/rendering.dart';
 
 class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
@@ -11,167 +12,225 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  List data = [];
-  List<int> selected = [];
-  bool _inSelection = false;
+  List<int> test = [0,1,2,3];
+  bool mode = true;
+  bool _isClock = true;
+  List<Color> _colorList = [Colors.red,Colors.blue,Colors.green,Colors.yellow];
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "floating button",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("App exist"),
-          actions: <Widget>[
-            unSelection(),
-            Center(child: Text(retSelected(),style: TextStyle(fontSize: 20.0),textAlign: TextAlign.center)),
-            IconButton(
-                onPressed: () {
-                  setState(() {
-                    deleteTiles();
-                  });
-                },
-                icon: selected.length > 0
-                    ? Icon(Icons.delete, color: Colors.white)
-                    : Icon(Icons.delete_outline, color: Colors.black)),
+
+    TextStyle appBarContStyle = TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.bold,
+      fontSize: 17.0,
+      letterSpacing: 1.5
+    );
+    TextStyle tapAppbarContStyle = TextStyle(
+        color: mode ? Colors.white:Colors.black,
+        fontWeight: FontWeight.bold,
+        fontSize: 17.0,
+        letterSpacing: 1.5
+    );
+    TextStyle timeAppbarContStyle = TextStyle(
+        color: mode ? Colors.black : Colors.white,
+        fontWeight: FontWeight.bold,
+        fontSize: 17.0,
+        letterSpacing: 1.5
+    );
+
+
+    return Scaffold(
+        body: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  constraints: BoxConstraints.expand(height: 50.0,width: getScreenWidth(context)-2),
+                  margin: const EdgeInsets.fromLTRB(1, 31, 1, 5),
+                  decoration: BoxDecoration(
+                    color: Colors.white70,
+                    border: Border.all(
+                      color: Colors.white70,
+                      width: 1
+                    ),
+                    borderRadius: BorderRadius.circular(12)
+                  ),
+                  child: Row(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.grey[100]
+                        ),
+                        constraints: BoxConstraints.expand(height: 35.0,width: 120.0),
+                        margin: EdgeInsets.only(left: 10),
+                        child: Center(
+                            child: Text(
+                              "Mode",
+                              style: appBarContStyle,
+                            )
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: mode ? Colors.blue[500] : Colors.grey[90]
+                        ),
+                        constraints: BoxConstraints.expand(height: 35.0,width: 120.0),
+                        margin: EdgeInsets.only(left: 5),
+                        child: Center(
+                          child: TextButton(
+                            child: Text(
+                              "Tap",
+                              style: tapAppbarContStyle,
+                            ),
+                            onPressed: (){
+                              setState(() {
+                                mode = true;
+                              });
+
+                            },
+
+                          ),
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: mode ? Colors.grey[100] : Colors.blue[500]
+                        ),
+                        constraints: BoxConstraints.expand(height: 35.0,width: 120.0),
+                        margin: EdgeInsets.only(left: 5),
+                        child: Center(
+                          child: TextButton(
+                            child: Text(
+                              "Time",
+                              style: timeAppbarContStyle,
+                            ),
+                            onPressed: (){
+                              setState(() {
+                                mode = false;
+                                rotateOnTime(Duration(seconds: 1));
+                              });
+                            },
+
+                          ),
+                        ),
+                      )
+
+                    ],
+                  ),
+                )
+              ],
+              mainAxisAlignment: MainAxisAlignment.start,
+            ),
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        // the boxes
+                        retColorBox(_colorList[0]),
+                        retColorBox(_colorList[1])
+                      ],
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        // the boxes
+                        retColorBox(_colorList[3]),
+                        retColorBox(_colorList[2])
+                      ],
+                    )
+                  ],
+                ),
+                Center(
+                  child: SizedBox(
+                    height: 92,
+                    width: 92,
+                    child: IconButton(
+                      icon: Image(
+                        image: _isClock ? AssetImage("images/reload.png") : AssetImage("images/reloadflipped.png"),
+                      ),
+                      onPressed: (){
+                        setState(() {
+                          _isClock = !_isClock;
+                        });
+
+                      },
+                    )
+                  ),
+                )
+              ],
+            )
           ],
-        ),
-        body: SingleChildScrollView(
-            child: Column(
-          textDirection: TextDirection.ltr,
           mainAxisAlignment: MainAxisAlignment.start,
-          children: generateChildren(context),
-        )),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              addIntoData(); //add data into data array
-            });
-          },
-          child: const Icon(Icons.add_box_rounded),
-          backgroundColor: Colors.black,
         ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-      ),
+      );
+  }
+
+  double getScreenWidth(BuildContext context){
+    return MediaQuery.of(context).size.width;
+  }
+
+  Widget retColorBox(Color coled){
+    return SizedBox(
+      width: 190,
+      height: 360,
+      child: ColoredBox(
+        color: coled,
+        child: TextButton(
+          child: Text(""),
+          onPressed: (){
+            if(mode){
+              setState(() {
+                rotateList();
+              });
+            }
+          },
+        ),
+      )
     );
   }
 
-  Widget unSelection(){
-    if(_inSelection){
-      return IconButton(
-          onPressed: () {
-            setState(() {
-              _inSelection = false;
-              selected.clear();
-            });
-          },
-          icon: Icon(Icons.clear)
-      );
-    }
-    return Text("");
+  void rotateOnTime(Duration duration){
+      Timer.periodic(duration, (timer) {
+        if(mode){
+          timer.cancel();
+        }
+        setState(() {
+          rotateList();
+        });
+
+      });
   }
 
-  String retSelected(){
-    if(_inSelection){
-      if(selected.length > 0)
-        return selected.length.toString();
-    }
-    return "";
-  }
+  void rotateList(){
+    List<Color> temp1 = _colorList;
+    if(_isClock){
+      Color temp = temp1[temp1.length-1];
+      for(int i=0;i<4;i++){
+          Color t = temp1[i];
+          temp1[i] = temp;
+          temp = t;
 
-  void addIntoData() {
-    data.add(new TileData(_getRandomStrings(), _getRandomStrings()));
-  }
-
-  List<Widget> generateChildren(BuildContext context) {
-    if (data.length == 0) {
-      return [];
-    }
-
-    List<Widget> returningList = [];
-    for (int i = 0; i < data.length; i++) {
-      TileData temp = data[i];
-      returningList.add(ListTile(
-        title: Text(temp.title),
-        subtitle: Text(temp.subtitle),
-        trailing: Icon(Icons.arrow_forward),
-        leading: iconInSelection(i),
-        selectedTileColor: Colors.grey,
-        selected: selected.contains(i),
-        onTap: () {
-          if (_inSelection) {
-            setState(() {
-              selectTile(i);
-            });
-          } else {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => Screen(temp.title, temp.subtitle))).then((value) {
-                        setState(() {
-                          if (value)
-                            data.removeAt(i);
-                        });
-              });
-          }
-        },
-        onLongPress: () {
-          setState(() {
-            if (!_inSelection) {
-              _inSelection = true;
-              // select function
-              selectTile(i);
-            }
-          });
-        },
-      ));
-    }
-    return returningList;
-  }
-
-  String _getRandomStrings() {
-    const chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz';
-    const int len = 5;
-    var rand = Random();
-    var llp = Iterable.generate(
-        len, (_) => chars.codeUnitAt(rand.nextInt(chars.length)));
-    return String.fromCharCodes(llp);
-  }
-
-  void selectTile(int index) {
-    if (selected.contains(index)) {
-      selected.remove(index);
-    } else {
-      selected.add(index);
-    }
-  }
-
-  Icon iconInSelection(int index) {
-    if (_inSelection) {
-      return Icon(selected.contains(index)
-          ? Icons.check_circle
-          : Icons.check_circle_outline);
-    } else {
-      return Icon(Icons.account_circle_outlined);
-    }
-  }
-
-  void deleteTiles() {
-    if (selected.length > 0) {
-      //need to be modified
-      for (int i = 0; i < selected.length; i++) {
-        data[selected[i]] = null;
       }
-      data.removeWhere((element) => element == null);
-      selected.clear();
-      _inSelection = false;
+      _colorList = temp1;
+    }
+    else{
+      Color temp = temp1[0];
+      for(int i=0;i<temp1.length;i++){
+        if(temp1.length-1 == i){
+          _colorList[i] = temp;
+        }else {
+          _colorList[i] = temp1[(i + 1) % temp1.length];
+        }
+      }
     }
   }
-}
-
-class TileData {
-  late String title;
-  late String subtitle;
-
-  TileData(this.title, this.subtitle);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'App.dart';
+
 void main() {
   runApp(MaterialApp(
+    title: "color shift app",
     home: App(),
+    debugShowCheckedModeBanner: false,
   ),
   );
 }
